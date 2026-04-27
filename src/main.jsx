d13399045c675b65cebd5fa3140176e7481bde94
Chrisearnz <103821630+Chrisearnz@users.noreply.github.com>
2026-04-27 14:38:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,3 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-html, body, #root {
-  margin: 0;
-  padding: 0;
-  background: #020617;
-  min-height: 100%;
-}
 import React from "react";
 import ReactDOM from "react-dom/client";
 import FateGraph from "./FateGraph.jsx";
