7b35f85ed611245306b18d760840bdb5468fd5b4
Chrisearnz <103821630+Chrisearnz@users.noreply.github.com>
2026-04-27 14:38:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,13 @@ html, body, #root {
   background: #020617;
   min-height: 100%;
 }
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FateGraph from "./FateGraph.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FateGraph />
+  </React.StrictMode>
+);
